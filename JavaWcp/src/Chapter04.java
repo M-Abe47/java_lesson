a7f2946b6f269aaf995d7109e104fbdcd611544d
M-Abe47 <abuzhengze5@gmail.com>
2021-08-21 18:20:09 +0900

    Finish chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+
+    public static void main(String[] args) {
+    	final int num1 = 10;
+        String str1 = "Abe_M";
+        System.out.println(str1);
+        System.out.println(num1);
+        str1 = "M_Abe"; // 再代入
+        System.out.println(str1);
+    }
+}
