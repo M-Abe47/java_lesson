3e32d51ea69f0b762c7c6b7143d3c9e00c1b7c09
M-Abe47 <abuzhengze5@gmail.com>
2021-09-02 20:07:26 +0900

    文字列の数値化

---
+++ JavaWcp/src/doko02_13.java
@@ -0,0 +1,7 @@
+public class doko02_13 {
+    public static void main(String[] args) {
+        String age = "24";
+        int n = Integer.parseInt(age);
+        System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+    }
+}
